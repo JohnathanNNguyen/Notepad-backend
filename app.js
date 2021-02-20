8d1eb226727cf9dfbcb38a303d206fed944dc8f5
Johnathan Nguyen <haungocvo@gmail.com>
2021-02-19 21:03:46 -0800

    made a connection to mySQL and created post/get request

--- app.js
+++ app.js
@@ -7,9 +7,37 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
 
-const pool = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+});
+
+app.get("/select", (req, res) => {
+  db.query("SELECT * FROM note_data", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
+
+app.post("/insert", (req, res) => {
+  const title = "new Title";
+  const content = "new content!!";
+  db.query(
+    "INSERT INTO note_data (title, content)VALUES (?, ?)",
+    [title, content],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(result);
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log("server running");
 });
